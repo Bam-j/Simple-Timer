5a137e47bf368146d31e25618a57531b57afe4c7
bam <juhyun19981129@gmail.com>
2023-02-08 18:20:33 +0900

    start 액션에 대한 폼 요소  엘리먼트 취득

--- js/ButtonActions/startAndPauseButtonAction.js
+++ js/ButtonActions/startAndPauseButtonAction.js
@@ -1,10 +1,20 @@
-export const startAndPauseButtonAction = (startAndPauseButton, time) => {
-  document.getElementById('inputHour').disabled = true;
-  document.getElementById('inputMinute').disabled = true;
-  document.getElementById('inputSecond').disabled = true;
+export const startAndPauseButtonAction = startAndPauseButton => {
+  const hour = document.getElementById('inputHour');
+  const minute = document.getElementById('inputMinute');
+  const second = document.getElementById('inputSecond');
+
+  let time = [parseInt(hour.value), parseInt(minute.value), parseInt(second.value)];
+  let totalSeconds = 0;
+
+  hour.disabled = true;
+  minute.disabled = true;
+  second.disabled = true;
 
   if (startAndPauseButton.innerText === 'start') {
     startAndPauseButton.innerText = 'pause';
+
+    totalSeconds = time[0] * 60 * 60 + time[1] * 60 + time[2];
+
   }
   else {
     startAndPauseButton.innerText = 'start';
--- js/index.js
+++ js/index.js
@@ -5,14 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const startAndPauseButton = document.getElementById('startAndPauseButton');
   const resetButton = document.getElementById('resetButton');
 
-  const hour = parseInt(document.getElementById('inputHour').value);
-  const minute = parseInt(document.getElementById('inputMinute').value);
-  const second = parseInt(document.getElementById('inputSecond').value);
-
-  let time = [hour, minute, second];
-
   startAndPauseButton.addEventListener('click', () => {
-    startAndPauseButtonAction(startAndPauseButton, time);
+    startAndPauseButtonAction(startAndPauseButton);
   });
   resetButton.addEventListener('click', () => {
     resetButtonAction();
