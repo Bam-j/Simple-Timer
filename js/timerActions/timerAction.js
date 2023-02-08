9787137277a68410b4a52a87800e7a46ab0e5b67
bam <juhyun19981129@gmail.com>
2023-02-08 19:10:47 +0900

    update 함수 구현과 TODO 추가

--- js/timerActions/timerAction.js
+++ js/timerActions/timerAction.js
@@ -5,16 +5,19 @@ export const timerAction = totalSeconds => {
   const minute = document.getElementById('inputMinute');
   const second = document.getElementById('inputSecond');
 
-  setInterval(() => {
+  let timerInterval = setInterval(() => {
     totalSeconds--;
     updateInput(totalSeconds);
 
+    //TODO: 0초가 되어도 alert는 뜨지만 second가 1로 남아있는 문제
     if (totalSeconds <= 0) {
       window.alert('시간종료');
 
       hour.disabled = false;
       minute.disabled = false;
       second.disabled = false;
+
+      clearInterval(timerInterval);
     }
   }, 1000);
 };
--- js/timerActions/updateInput.js
+++ js/timerActions/updateInput.js
@@ -1,3 +1,14 @@
 export const updateInput = totalSeconds => {
+  //TODO: 엘리먼트 취득하는 중복 코드 줄이기
+  const hour = document.getElementById('inputHour');
+  const minute = document.getElementById('inputMinute');
+  const second = document.getElementById('inputSecond');
 
+  const currentHour = ~~(totalSeconds / 60 / 60);
+  const currentMinute = ~~((totalSeconds / 60) % 60);
+  const currentSecond = totalSeconds % 60;
+
+  hour.value = currentHour;
+  minute.value = currentMinute;
+  second.value = currentSecond;
 };
