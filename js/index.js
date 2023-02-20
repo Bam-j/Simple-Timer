87b66f67c74e7a320863becffc22275146238ad0
bam <juhyun19981129@gmail.com>
2023-02-20 23:52:12 +0900

    다크 모드 버튼 동작 구현 시작

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 import {startAndPauseButtonAction} from './ButtonActions/startAndPauseButtonAction.js';
 import {resetButtonAction} from './ButtonActions/resetButtonAction.js';
+import {darkModeButtonAction} from './ButtonActions/darkModeButtonAction.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const startAndPauseButton = document.getElementById('startAndPauseButton');
   const resetButton = document.getElementById('resetButton');
+  const darkModeButton = document.getElementById('dark-mode-button');
 
   startAndPauseButton.addEventListener('click', () => {
     startAndPauseButtonAction(startAndPauseButton);
   });
+
   resetButton.addEventListener('click', () => {
     resetButtonAction();
     startAndPauseButton.innerText = 'start';
   });
+
+  darkModeButton.addEventListener('click', () => {
+    darkModeButtonAction();
+  })
 });
