8a6921273f97ff1c60dc2101676308528cc66102
bam <juhyun19981129@gmail.com>
2023-02-21 19:01:52 +0900

    다크모드 버튼 동작 완성

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const startAndPauseButton = document.getElementById('startAndPauseButton');
   const resetButton = document.getElementById('resetButton');
   const darkModeButton = document.getElementById('dark-mode-button');
+  const darkModeImg = document.getElementById('dark-mode-img');
+
+  let currentMode = 'light';
 
   startAndPauseButton.addEventListener('click', () => {
     startAndPauseButtonAction(startAndPauseButton);
@@ -20,9 +23,31 @@ document.addEventListener('DOMContentLoaded', () => {
   darkModeButton.addEventListener('click', e => {
     if (e.target.checked) {
       document.body.classList.add('dark-mode');
+      darkModeImg.src = '/icons/moon.svg';
+      currentMode = 'dark';
     }
     else {
       document.body.classList.remove('dark-mode');
+      darkModeImg.src = '/icons/brightness-high.svg';
+      currentMode =  'light';
+    }
+  });
+
+  darkModeImg.addEventListener('mouseover', () => {
+    if (currentMode === 'light') {
+      darkModeImg.src = '/icons/brightness-high-fill.svg';
+    }
+    else {
+      darkModeImg.src = '/icons/moon-fill.svg';
+    }
+  });
+
+  darkModeImg.addEventListener('mouseout', () => {
+    if (currentMode === 'light') {
+      darkModeImg.src = '/icons/brightness-high.svg';
+    }
+    else {
+      darkModeImg.src = '/icons/moon.svg';
     }
   });
 });
