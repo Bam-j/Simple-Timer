cb078b556214159bf08303db33cc5688917aded3
bam <juhyun19981129@gmail.com>
2023-02-21 23:11:28 +0900

    다크모드 완성

--- js/ButtonActions/changeDarkMode.js
+++ js/ButtonActions/changeDarkMode.js
@@ -1,6 +1,12 @@
 export const changeDarkMode = () => {
   document.body.classList.add('dark-mode');
+
+  document.getElementById('dark-mode-img').src = '/icons/moon-fill.svg';
+
+  document.getElementById('input-hour').classList.add('dark-mode');
+  document.getElementById('input-minute').classList.add('dark-mode');
+  document.getElementById('input-second').classList.add('dark-mode');
+
   document.getElementById('startAndPauseButton').classList.add('dark-mode');
   document.getElementById('resetButton').classList.add('dark-mode');
-  document.getElementById('dark-mode-img').src = '/icons/moon-fill.svg';
 };
